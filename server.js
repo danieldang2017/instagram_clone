105e92b9d69d07cc96a7c2bd5f214447f0769bc9
Daniel Dang <danieldang2017@gmail.com>
2017-06-18 23:49:53 +0000

    Fixed bugs and checked all display

--- server.js
+++ server.js
@@ -1,61 +1,68 @@
-// #NodeJS server
-//
-// A simple server to serve the Instagram clone website.
-//
-
-
+/*#NodeJS server
+  A simple server to serve the Instagram clone website
+--------------------------------- Server configuration ----------------------------------
+  1. Adds external modules from node_modules
+*/
 var http = require('http');
 var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// 2. Adds schemas from models
 var Post = require('./models/Post.js');
 var User = require('./models/User.js');
-var express = require('express');
-var mongoose = require('mongoose');
+
+// 3. Establishes connection to the MongoDB 
 mongoose.connect('mongodb://instaadmin1:webadmin123@ds119682.mlab.com:19682/instagramdb')
 
-//
-// ## NodeJS server `NodeJSServer(obj)`
-//
-// Creates a new instance of NodeJS server with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
+// 4. Creates a new instance of NodeJS router and server
 var router = express();
 var server = http.createServer(router);
-//tell the router (ie. express) where to find static files
+
+// 5. Tells the router where to find static files
 router.use(express.static(path.resolve(__dirname, 'client')));
-//tell the router to parse JSON data for us and put it into req.body
-//router.use(express.bodyParser());
 
+// 6. Tells the router to parse JSON data and put it into req.body
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-router.post('/loadUser', (req, res) => {
-  User.find({userName: 'Daniel'})
+/*
+-------------------------------- Processes requests from client ------------------------------
+
+  I. Index.html
+  
+    1. Load user profile
+*/
+router.post('/getUserProfile', (req, res) => {
+  User.findById(req.body.id)
   .then((user) => {
-  //  console.log(user);
     res.json(user);
   });
 });
 
-// 20170613 by Winson
-// Load user information for main page
-router.post('/loadUserPost', function(req, res){
-   User.find({_id: req.body.id})
-  .then((user) => {
-    console.log(user);
-    res.json(user);
+//  2. Load top profiles
+router.post('/getTopProfiles', function(req, res){
+   User.find().sort({followersCount : -1}).limit(2)
+  .then((profiles) => {
+    res.json(profiles);
   });
 });
 
-// 20170613 by Winson
-// Load post information by userID for main page
-router.post('/loadPost', function(req, res){
-   Post.find({userId: req.body.id})
- // Post.find()
-  .then((post) => {
-    console.log(post);
-    res.json(post);
+//  3. Load top posts
+router.post('/getPostsContent', function(req, res){
+   Post.find({ "userId" : req.body.id}).limit(4)
+  .then((postsContent) => {
+    res.json(postsContent);
+    console.log(postsContent);
   });
 });
 
-
+/*---------------------------------------------------------------------------------------
+  Starts server
+*/
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("NodeJS server listening at", addr.address + ":" + addr.port);
